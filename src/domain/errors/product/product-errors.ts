aa18ac04821b782a17044030c16c1b36ba67b167
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-28 11:50:45 -0300

    feat: add product specific errors

---
+++ src/domain/errors/product/product-errors.ts
@@ -0,0 +1,25 @@
+import { DomainError } from "../domain-errors.js"
+
+export class InvalidNameError extends DomainError {
+  constructor() {
+    super("Invalid name")
+  }
+}
+
+export class InvalidPriceError extends DomainError {
+  constructor() {
+    super("Invalid price")
+  }
+}
+
+export class InvalidTypeError extends DomainError {
+  constructor() {
+    super("Invalid type")
+  }
+}
+
+export class InvalidExpirationDateError extends DomainError {
+  constructor() {
+    super("Invalid expiration date")
+  }
+}
