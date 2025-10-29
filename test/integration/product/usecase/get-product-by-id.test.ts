07bbe691b305151f5636b3d67ac7d6d47440621d
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-29 12:08:57 -0300

    test: ensure GetProductById use case get a product by id successfully

---
+++ test/integration/product/usecase/get-product-by-id.test.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client"
+
+import CreateProduct from "../../../../src/application/usecase/product/create-product.js"
+import GetProductById from "../../../../src/application/usecase/product/get-pet-by-id.js"
+import { startPostgresTestDb } from "../../../../src/infra/database/test-db.js"
+import ProductRepositoryDatabase from "../../../../src/infra/repository/product/product-repository.js"
+
+describe("Get Product By Id Use Case", () => {
+  let prisma: PrismaClient
+  let repository: ProductRepositoryDatabase
+  let getProductById: GetProductById
+  let createProduct: CreateProduct
+
+  beforeAll(async () => {
+    const ctx = await startPostgresTestDb()
+    prisma = ctx.prisma
+    repository = new ProductRepositoryDatabase(prisma)
+    getProductById = new GetProductById(repository)
+    createProduct = new CreateProduct(repository)
+  })
+
+  afterEach(async () => {
+    await prisma.product.deleteMany({})
+  })
+
+  test("should get a product by id successfully", async () => {
+    const createProductInput = {
+      name: "Test Product",
+      description: "This is a test product",
+      type: "OTHER",
+      priceInCents: 1500,
+      promoInCents: 1200,
+      promoActive: true,
+      promoStartsAt: new Date("2024-07-01"),
+      promoEndsAt: new Date("2024-07-31"),
+      stockQuantity: 100,
+      expiresAt: new Date("2025-12-01"),
+    }
+    const createProductOuput = await createProduct.execute(createProductInput)
+    const getProductInput = {
+      productId: createProductOuput.productId,
+    }
+    const getProductOutput = await getProductById.execute(getProductInput)
+    expect(getProductOutput.productId).toBe(createProductOuput.productId)
+    expect(getProductOutput.name).toBe(createProductInput.name)
+    expect(getProductOutput.description).toBe(createProductInput.description)
+    expect(getProductOutput.type).toBe(createProductInput.type)
+    expect(getProductOutput.priceInCents).toBe(createProductInput.priceInCents)
+    expect(getProductOutput.stockQuantity).toBe(
+      createProductInput.stockQuantity
+    )
+    expect(getProductOutput.promoActive).toBe(createProductInput.promoActive)
+    expect(getProductOutput.promoInCents).toBe(createProductInput.promoInCents)
+    expect(getProductOutput.promoStartsAt).toEqual(
+      createProductInput.promoStartsAt
+    )
+    expect(getProductOutput.promoEndsAt).toEqual(createProductInput.promoEndsAt)
+    expect(getProductOutput.expiresAt).toEqual(createProductInput.expiresAt)
+  })
+})
