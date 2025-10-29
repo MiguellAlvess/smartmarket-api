76226df04c08a9582df4344837203c72ccd708e7
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-29 11:55:05 -0300

    test: ensure product can be created in database

---
+++ test/integration/product/repository/product-repository.test.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client"
+
+import Product from "../../../../src/domain/product/entity/product.js"
+import { startPostgresTestDb } from "../../../../src/infra/database/test-db.js"
+import ProductRepositoryDatabase from "../../../../src/infra/repository/product/product-repository.js"
+
+describe("Product Repository Database", async () => {
+  let prisma: PrismaClient
+  let repository: ProductRepositoryDatabase
+
+  beforeAll(async () => {
+    const ctx = await startPostgresTestDb()
+    prisma = ctx.prisma
+    repository = new ProductRepositoryDatabase(prisma)
+  })
+
+  afterEach(async () => {
+    await prisma.product.deleteMany({})
+  })
+
+  test("should create a new product successfully in database", async () => {
+    const product = Product.create(
+      "Test Product",
+      "This is a test product",
+      "OTHER",
+      1500,
+      1200,
+      true,
+      new Date("2024-07-01"),
+      new Date("2024-07-31"),
+      100,
+      new Date("2025-12-01")
+    )
+    const output = await repository.create(product)
+    expect(output).toBeUndefined()
+    expect(product.getId()).toBeDefined()
+  })
+})
