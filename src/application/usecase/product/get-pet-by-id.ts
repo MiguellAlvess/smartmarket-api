903d88b57b3391b3c78985ba404a77dc2c4c3e68
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-29 12:09:19 -0300

    feat: add GetProductById use cases

---
+++ src/application/usecase/product/get-pet-by-id.ts
@@ -0,0 +1,43 @@
+import { ProductRepository } from "../../ports/repository/product-repository.js"
+
+export default class GetProductById {
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const product = await this.productRepository.findById(input.productId)
+    if (!product) {
+      throw new Error("Product not found")
+    }
+    return {
+      productId: product.getId(),
+      name: product.getName(),
+      description: product.getDescription(),
+      type: product.getType(),
+      priceInCents: product.getPriceInCents().getValue(),
+      stockQuantity: product.getStockQuantity(),
+      promoActive: product.isPromoActive(),
+      promoInCents: product.getPromoInCents()?.getValue(),
+      promoStartsAt: product.getPromoStartsAt(),
+      promoEndsAt: product.getPromoEndsAt(),
+      expiresAt: product.getExpiresAt(),
+    }
+  }
+}
+
+type Input = {
+  productId: string
+}
+
+type Output = {
+  productId: string
+  name: string
+  description: string
+  type: string
+  priceInCents: number
+  stockQuantity: number
+  promoActive: boolean
+  promoInCents?: number
+  promoStartsAt?: Date
+  promoEndsAt?: Date
+  expiresAt?: Date
+}
