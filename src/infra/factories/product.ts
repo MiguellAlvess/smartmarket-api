e6f9fb4035c52524fe37b668ae70792ba4b7c9b5
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-01 12:00:39 -0300

    feat: add DELETE /me/:productId route

--- src/infra/factories/product.ts
+++ src/infra/factories/product.ts
@@ -1,6 +1,8 @@
 import CreateProduct from "../../application/usecase/product/create-product.js"
+import DeleteProduct from "../../application/usecase/product/delete-product.js"
 import GetProductById from "../../application/usecase/product/get-pet-by-id.js"
 import CreateProductController from "../controller/product/create-product.js"
+import DeleteProductController from "../controller/product/delete-product.js"
 import GetProductByIdController from "../controller/product/get-product-by-id.js"
 import { prisma } from "../database/prisma.js"
 import ProductRepositoryDatabase from "../repository/product/product-repository.js"
@@ -14,11 +16,20 @@ export const makeCreateProductController = () => {
   return createProductController
 }
 
-const makeGetProductByIdController = () => {
+export const makeGetProductByIdController = () => {
   const productRepository = new ProductRepositoryDatabase(prisma)
   const getProductByIdUseCase = new GetProductById(productRepository)
   const getProductByIdController = new GetProductByIdController(
     getProductByIdUseCase
   )
   return getProductByIdController
+}
+
+export const makeDeleteProductController = () => {
+  const productRepository = new ProductRepositoryDatabase(prisma)
+  const deleteProductUseCase = new DeleteProduct(productRepository)
+  const deleteProductController = new DeleteProductController(
+    deleteProductUseCase
+  )
+  return deleteProductController
 }
--- src/infra/routes/product-routes.ts
+++ src/infra/routes/product-routes.ts
@@ -1,13 +1,31 @@
 import { Router } from "express"
 
-import { makeCreateProductController } from "../factories/product.js"
+import {
+  makeCreateProductController,
+  makeDeleteProductController,
+  makeGetProductByIdController,
+} from "../factories/product.js"
 
 export const productRouter = Router()
 
 productRouter.post("/", async (req, res) => {
-  const controller = makeCreateProductController()
-  const { statusCode, body } = await controller.execute(req)
+  const createProductController = makeCreateProductController()
+  const { statusCode, body } = await createProductController.execute(req)
   res.status(statusCode).send(body)
 })
 
-productRouter.get("/:productId", async (req, res) => {})
+productRouter.get("/:productId", async (req, res) => {
+  const getProductByIdController = makeGetProductByIdController()
+  const { statusCode, body } = await getProductByIdController.execute({
+    params: req.params,
+  })
+  res.status(statusCode).send(body)
+})
+
+productRouter.delete("/me/:productId", async (req, res) => {
+  const deleteProductController = makeDeleteProductController()
+  const { statusCode, body } = await deleteProductController.execute({
+    params: req.params,
+  })
+  res.status(statusCode).send(body)
+})
