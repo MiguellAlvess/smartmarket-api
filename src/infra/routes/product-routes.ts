71070b40e632a565f05cd0298ecc1c482a8a916a
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-01 17:18:30 -0300

    fix: correct product listing route to avoid invalid UUID error

--- src/infra/routes/product-routes.ts
+++ src/infra/routes/product-routes.ts
@@ -9,9 +9,9 @@ import {
 
 export const productRouter = Router()
 
-productRouter.post("/", async (req, res) => {
-  const createProductController = makeCreateProductController()
-  const { statusCode, body } = await createProductController.execute(req)
+productRouter.get("/", async (req, res) => {
+  const getAllProductsController = makeGetAllProductsController()
+  const { statusCode, body } = await getAllProductsController.execute()
   res.status(statusCode).send(body)
 })
 
@@ -23,16 +23,16 @@ productRouter.get("/:productId", async (req, res) => {
   res.status(statusCode).send(body)
 })
 
+productRouter.post("/", async (req, res) => {
+  const createProductController = makeCreateProductController()
+  const { statusCode, body } = await createProductController.execute(req)
+  res.status(statusCode).send(body)
+})
+
 productRouter.delete("/me/:productId", async (req, res) => {
   const deleteProductController = makeDeleteProductController()
   const { statusCode, body } = await deleteProductController.execute({
     params: req.params,
   })
-  res.status(statusCode).send(body)
-})
-
-productRouter.get("/", async (req, res) => {
-  const getAllProductsController = makeGetAllProductsController()
-  const { statusCode, body } = await getAllProductsController.execute()
   res.status(statusCode).send(body)
 })
