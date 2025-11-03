c07a207404b0377e379d6cb9d5c971b09c364276
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-03 17:59:10 -0300

    refactor: rename passwordHash for password

--- src/domain/account/entity/account.ts
+++ src/domain/account/entity/account.ts
@@ -1,30 +1,28 @@
 import UUID from "../../product/vo/uuid.js"
 import Cpf from "../vo/cpf.js"
 import Email from "../vo/email.js"
+import Name from "../vo/name.js"
 import { Password } from "../vo/password.js"
 
 export default class Account {
   private accountId: UUID
-  private name: string
+  private name: Name
   private email: Email
   private cpf: Cpf
-  private passwordHash: Password
-  private imageUrl?: string
+  private password: Password
 
   constructor(
     accountId: string,
     name: string,
     email: string,
     cpf: string,
-    passwordHash: string,
-    imageUrl?: string
+    passwordHash: string
   ) {
     this.accountId = new UUID(accountId)
-    this.name = name
+    this.name = new Name(name)
     this.email = new Email(email)
     this.cpf = new Cpf(cpf)
-    this.passwordHash = new Password(passwordHash)
-    this.imageUrl = imageUrl
+    this.password = new Password(passwordHash)
   }
 
   static create(
@@ -35,7 +33,7 @@ export default class Account {
     imageUrl?: string
   ) {
     const accountId = UUID.create().getValue()
-    return new Account(accountId, name, email, cpf, passwordHash, imageUrl)
+    return new Account(accountId, name, email, cpf, passwordHash)
   }
 
   getId() {
@@ -43,7 +41,7 @@ export default class Account {
   }
 
   getName() {
-    return this.name
+    return this.name.getValue()
   }
 
   getEmail() {
@@ -55,10 +53,6 @@ export default class Account {
   }
 
   getPasswordHash() {
-    return this.passwordHash.getValue()
-  }
-
-  getImageUrl() {
-    return this.imageUrl
+    return this.password.getValue()
   }
 }
