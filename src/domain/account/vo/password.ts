91830df6c081464e06b399013a521a66d94d162b
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-03 19:15:07 -0300

    fix: allow Password VO to accept hashed passwords via fromHash method

--- src/domain/account/vo/password.ts
+++ src/domain/account/vo/password.ts
@@ -3,13 +3,23 @@ import { InvalidPasswordError } from "../../errors/account/account-errors.js"
 export class Password {
   private value: string
 
-  constructor(password: string) {
-    if (!this.validatePassword(password)) throw new InvalidPasswordError()
+  private constructor(password: string, skipValidation = false) {
+    if (!skipValidation && !this.validatePassword(password)) {
+      throw new InvalidPasswordError()
+    }
     this.value = password
   }
 
-  validatePassword(password: string) {
-    return password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
+  static create(plain: string): Password {
+    return new Password(plain)
+  }
+
+  static fromHash(hash: string): Password {
+    return new Password(hash, true)
+  }
+
+  private validatePassword(password: string) {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password)
   }
 
   getValue() {
