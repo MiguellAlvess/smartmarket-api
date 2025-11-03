f370ced665f0d9ddb12d3d78c46c8309878d12fa
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-03 19:15:20 -0300

    feat: update Account entity to accept Password VO instead of string

--- src/domain/account/entity/account.ts
+++ src/domain/account/entity/account.ts
@@ -1,55 +1,46 @@
 import UUID from "../../product/vo/uuid.js"
-import Cpf from "../vo/cpf.js"
-import Email from "../vo/email.js"
-import Name from "../vo/name.js"
 import { Password } from "../vo/password.js"
 
-export default class Account {
-  private accountId: UUID
-  private name: Name
-  private email: Email
-  private cpf: Cpf
+export class Account {
+  private id: UUID
+  private name: string
+  private email: string
+  private cpf: string
   private password: Password
 
   constructor(
-    accountId: string,
+    id: string,
     name: string,
     email: string,
     cpf: string,
-    passwordHash: string
+    password: Password
   ) {
-    this.accountId = new UUID(accountId)
-    this.name = new Name(name)
-    this.email = new Email(email)
-    this.cpf = new Cpf(cpf)
-    this.password = new Password(passwordHash)
+    this.id = new UUID(id)
+    this.name = name
+    this.email = email
+    this.cpf = cpf
+    this.password = password
   }
 
-  static create(
-    name: string,
-    email: string,
-    cpf: string,
-    passwordHash: string,
-    imageUrl?: string
-  ) {
-    const accountId = UUID.create().getValue()
-    return new Account(accountId, name, email, cpf, passwordHash)
+  static create(name: string, email: string, cpf: string, password: Password) {
+    const id = UUID.create().getValue()
+    return new Account(id, name, email, cpf, password)
   }
 
   getId() {
-    return this.accountId.getValue()
+    return this.id.getValue()
   }
 
   getName() {
-    return this.name.getValue()
+    return this.name
   }
 
   getEmail() {
-    return this.email.getValue()
+    return this.email
   }
 
   getCpf() {
-    return this.cpf.getValue()
+    return this.cpf
   }
 
   getPasswordHash() {
