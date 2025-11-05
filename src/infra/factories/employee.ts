690da93b2d732ebe018ca6944417002b782fe113
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-05 08:00:19 -0300

    feat: add GET /api/employees/:employeeId route

--- src/infra/factories/employee.ts
+++ src/infra/factories/employee.ts
@@ -1,5 +1,7 @@
 import CreateEmployeeUseCase from "../../application/usecase/employee/create-employee.js"
+import GetEmployeeByIdUseCase from "../../application/usecase/employee/get-employee-by-id.js"
 import CreateEmployeeController from "../controller/employee/create-employee.js"
+import GetEmployeeByIdController from "../controller/employee/get-employee-by-id.js"
 import { prisma } from "../database/prisma.js"
 import EmployeeRepositoryDatabase from "../repository/employee/employee-repository.js"
 
@@ -10,4 +12,13 @@ export const makeCreateEmployeeController = () => {
     createEmployeeUseCase
   )
   return createEmployeeController
+}
+
+export const makeGetEmployeeByIdController = () => {
+  const employeeRepository = new EmployeeRepositoryDatabase(prisma)
+  const getEmployeeByIdUseCase = new GetEmployeeByIdUseCase(employeeRepository)
+  const getEmployeeByIdController = new GetEmployeeByIdController(
+    getEmployeeByIdUseCase
+  )
+  return getEmployeeByIdController
 }
--- src/infra/routes/employee-routes.ts
+++ src/infra/routes/employee-routes.ts
@@ -1,11 +1,22 @@
 import { Router } from "express"
 
 import { makeAuth } from "../factories/auth.js"
-import { makeCreateEmployeeController } from "../factories/employee.js"
+import {
+  makeCreateEmployeeController,
+  makeGetEmployeeByIdController,
+} from "../factories/employee.js"
 
 export const employeeRouter = Router()
 const auth = makeAuth()
 employeeRouter.use(auth)
+
+employeeRouter.get("/:employeeId", async (req, res) => {
+  const getEmployeeByIdController = makeGetEmployeeByIdController()
+  const { statusCode, body } = await getEmployeeByIdController.execute({
+    params: req.params,
+  })
+  res.status(statusCode).send(body)
+})
 
 employeeRouter.post("/", async (req, res) => {
   const createEmployeeController = makeCreateEmployeeController()
