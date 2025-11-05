9d2ec3cbbebe2cb445d43c7ee1916a3bc3e25d01
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-05 07:50:02 -0300

    feat: add findById method for EmployeeRepositoryDatabase

--- src/application/ports/repository/employee-repository.ts
+++ src/application/ports/repository/employee-repository.ts
@@ -3,4 +3,5 @@ import { Employee } from "../../../domain/employee/entity/employee.js"
 export interface EmployeeRepository {
   create(employee: Employee): Promise<void>
   findByCpf(cpf: string): Promise<Employee | null>
+  findById(employeeId: string): Promise<Employee | null>
 }
--- src/infra/repository/employee/employee-repository.ts
+++ src/infra/repository/employee/employee-repository.ts
@@ -6,6 +6,18 @@ import { Employee } from "../../../domain/employee/entity/employee.js"
 export default class EmployeeRepositoryDatabase implements EmployeeRepository {
   constructor(private readonly db: PrismaClient) {}
 
+  async create(employee: Employee): Promise<void> {
+    await this.db.employee.create({
+      data: {
+        id: employee.getId(),
+        name: employee.getName(),
+        cpf: employee.getCpf(),
+        age: employee.getAge(),
+        jobTitle: employee.getJobTitle(),
+      },
+    })
+  }
+
   async findByCpf(cpf: string): Promise<Employee | null> {
     const employeeRow = await this.db.employee.findUnique({ where: { cpf } })
     if (!employeeRow) return null
@@ -18,15 +30,17 @@ export default class EmployeeRepositoryDatabase implements EmployeeRepository {
     )
   }
 
-  async create(employee: Employee): Promise<void> {
-    await this.db.employee.create({
-      data: {
-        id: employee.getId(),
-        name: employee.getName(),
-        cpf: employee.getCpf(),
-        age: employee.getAge(),
-        jobTitle: employee.getJobTitle(),
-      },
+  async findById(employeeId: string): Promise<Employee | null> {
+    const employeeRow = await this.db.employee.findUnique({
+      where: { id: employeeId },
     })
+    if (!employeeRow) return null
+    return new Employee(
+      employeeRow.id,
+      employeeRow.name,
+      employeeRow.cpf,
+      employeeRow.age,
+      employeeRow.jobTitle
+    )
   }
 }
